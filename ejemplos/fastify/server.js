797fbb590945b7e2813c37722f5231dc6f2720f7
stgoneira <stgoneiran@gmail.com>
2023-04-13 00:18:55 -0400

    envío y recepción datos

--- ejemplos/fastify/server.js
+++ ejemplos/fastify/server.js
@@ -1,18 +1,29 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true });
 
+// CORS
+fastify.register( require('@fastify/cors'), {} );
+
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+  return { hello: 'world' };
+});
+
+// para probar en cliente recibir datos por GET y por POST 
+// fetch('http://localhost:3000/form', {method: 'POST', body: JSON.stringify({a:'uno', b:'dos'}), headers: {'Content-Type': 'application/json'} }).then(r=>r.json()).then(d=>console.dir(d));
+fastify.post('/form', async (request, reply) => {
+  const datosGET  = request.query;
+  const datosPOST = request.body;
+  return { datosGET, datosPOST };
+});
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000 });
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
 }
 start();
