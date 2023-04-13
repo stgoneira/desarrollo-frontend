50250ef1de0643a4ec29aca6ea83a76d0e45ec63
stgoneira <stgoneiran@gmail.com>
2023-04-13 17:24:11 -0400

    Firestore OK

--- ejemplos/fastify/src/firebase/firestore.js
+++ ejemplos/fastify/src/firebase/firestore.js
@@ -1,5 +1,52 @@
-const app = require('../../include/firebase.js');
+const { async } = require('@firebase/util');
+const { admin } = require('../../include/firebase.js');
+const db        = admin.firestore();
+const coleccion = db.collection('productos');
+
+// Source: https://googleapis.dev/nodejs/firestore/latest/DocumentReference.html 
 
 module.exports = async (req, res) => {
-    return {msje: 'TODO'};
+    switch(req.method) {
+        case 'GET':
+            //return buscar();
+            //return agregar();
+            //return modificar();
+            return borrar();
+        case 'POST':
+        case 'PUT':
+        case 'DELETE':
+        default:
+            res.code(405).send("Método HTTP no permitido");
+    }
 };
+
+async function buscar() {
+    const query     = await coleccion.where('precio', '<', 20000);
+    const productos = (await query.get()).docs.map(d => d.data());
+    return {productos};
+}
+
+async function agregar() {
+    const documento = {        
+        nombre: 'Producto 3',
+        precio: 990
+    };
+    const docRef = await coleccion.doc();
+    documento.id = docRef.id;
+    docRef.set(documento);
+    return documento;
+}
+
+async function modificar() {
+    const docRef = await coleccion.doc("lNp3wtBmGhQ98brEsrFg");
+    await docRef.update({
+        precio: 99990
+    });
+    return {docRef};
+}
+
+async function borrar() {
+    const docRef = await coleccion.doc("8apF9lGNoMq928a6QFGo");
+    await docRef.delete();
+    return {borrado: true};    
+}
