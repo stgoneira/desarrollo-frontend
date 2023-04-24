f8be1f44ba82edf0fabc24a315ad2e22430a82fb
hcslearning <learning@hardcybersoft.cl>
2023-04-23 23:55:45 -0400

    modules example

---
+++ ejemplos/js-modules/js-modules.mjs
@@ -0,0 +1,5 @@
+import { suma, resta } from './matematicas.mjs';
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    document.write("2 + 2 son "+suma(2, 2));
+});
---
+++ ejemplos/js-modules/matematicas.mjs
@@ -0,0 +1,10 @@
+function suma(n1, n2) {
+    return n1 + n2;
+} 
+function resta(n1, n2) {
+    return n1 - n2;
+} 
+export {
+    suma,
+    resta
+}
