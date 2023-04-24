350a3f1d8f7373e3ac819cf4660223fc7b770685
hcslearning <learning@hardcybersoft.cl>
2023-04-24 14:22:35 -0400

    ahora funcionando con Babel e imports

--- ejemplos/react-esm/saludo.js
+++ ejemplos/react-esm/saludo.js
@@ -1,3 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 export default function Saludo() {
-  return /*#__PURE__*/React.createElement("div", null, "Hola Mundo Externo!!!! ");
+  return /*#__PURE__*/_jsx("div", {
+    children: "Hola Mundo Externo!!!! "
+  });
 }
